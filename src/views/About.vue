5a78ad09b7b3172d4071012777df5ac4b0542314
Stephan Strittmatter <stritti@users.noreply.github.com>
2021-02-19 11:26:58 +0100

    update About

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,26 @@
 <template>
   <div class="about mt-5">
     <h1>Über das Projekt kauf2</h1>
-    <p>Kommt bald ...</p>
+    <p>
+      Dieses Projekt wurde als Idee 2015 gestartet und wird seitdem
+      immer wieder erfolgreich regional umgesetzt.
+    </p>
+    <p>
+      Diese Webseite wurde privat erstellt und wird auch privat betrieben.
+    </p>
+    <p>
+      Falls du den Betrieb dieser Webseite unterstützen möchtest:
+      <a
+        href="https://ko-fi.com/J3J33A8DT"
+        target="_blank"
+      ><img
+        height="36"
+        style="border:0px;height:36px;"
+        src="https://cdn.ko-fi.com/cdn/kofi2.png?v=2"
+        border="0"
+        alt="Buy Me a Coffee at ko-fi.com"
+      ></a>
+    </p>
   </div>
 </template>
 
