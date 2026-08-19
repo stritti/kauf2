572820eeff262c5b2e7ac3d31600645b8bdf4748
Stephan Strittmatter <stephan@st-strittmatter.name>
2026-08-19 14:24:48 +0200

    Fix product list bullet indentation in cards
    Wrap product <li> items in a proper <ul> instead of rendering them as
    orphan children of <p class="card-text">, which pushed the bullet
    markers to the card edge.

--- src/components/product/ProductList.vue
+++ src/components/product/ProductList.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    <b-card-text
+    <ul
       v-if="productList"
+      class="product-list"
     >
       <list-item
         v-for="product in productList"
         :key="product.id"
         :product="product"
       />
-    </b-card-text>
+    </ul>
     <div
       v-else
       class="d-flex align-items-center loading"
@@ -34,6 +35,10 @@ const { list: productList } = useAirtableList<WithId<Product>>(
 
 <style lang="scss">
 @import '@/assets/scss/custom-vars.scss';
+.product-list {
+  margin-bottom: 0;
+  padding-left: 1.5rem;
+}
 .loading {
   min-height: 200px;
   .spinner-border {
